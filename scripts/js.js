a1cfafe9f871359b505109e7d7a11f97b925ac04
Hideto Yasunori <hideto0118yasunori@gmail.com>
2016-12-03 23:30:04 -0800

    Erase =? from calculation

--- scripts/js.js
+++ scripts/js.js
@@ -13,7 +13,7 @@ module.exports = function (robot) {
     }
   });
 
-  robot.hear(/([0-9]+)([+\-*\/])([0-9]+)=\?/i, function (res) {
+  robot.hear(/([0-9]+)([+\-*\/])([0-9]+)/i, function (res) {
     var leftHand = res.match[1];
     var operator = res.match[2];
     var rightHand = res.match[3];
